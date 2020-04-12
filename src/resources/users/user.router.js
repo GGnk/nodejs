848bab0d20e0be18d04c6dbb5e7b9cff4f95a777
GGnk <super-nurlan94@yandex.ru>
2020-04-12 16:05:23 +0400

    rafactor: add error handler in user routers

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,32 +1,61 @@
 const router = require('express').Router();
+const createError = require('http-errors');
+const { BAD_REQUEST, NOT_FOUND, OK, NO_CONTENT } = require('http-status-codes');
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router.get('/', async (req, res) => {
-  const users = await usersService.getAll();
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await usersService.getAll();
+    res.status(OK).json(users.map(User.toResponse));
+  } catch (err) {
+    return next(err);
+  }
+});
+router.get('/:id', async (req, res, next) => {
+  try {
+    const userGetId = await usersService.getId(req.params.id);
+    if (!userGetId.id) {
+      throw new createError(NOT_FOUND, 'User not found');
+    } else {
+      res.status(OK).json(User.toResponse(userGetId));
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
 
-  res.json(users.map(User.toResponse));
+router.post('/', async (req, res, next) => {
+  try {
+    const userCreate = await usersService.postUser(req.body);
+    if (!userCreate) {
+      throw new createError(BAD_REQUEST, 'Bad data');
+    } else {
+      res.status(OK).json(User.toResponse(userCreate));
+    }
+  } catch (err) {
+    return next(err);
+  }
 });
-router.get('/:id', async (req, res) => {
-  const userGetId = await usersService.getId(req.params.id);
-  res.json(User.toResponse(userGetId));
+router.put('/:id', async (req, res, next) => {
+  try {
+    const userUpdate = await usersService.putUser(req.params.id, req.body);
+    if (userUpdate.id) {
+      res.status(OK).json(User.toResponse(userUpdate));
+    } else {
+      throw new createError(BAD_REQUEST, 'Bad data');
+    }
+  } catch (err) {
+    return next(err);
+  }
 });
-router.post('/', async (req, res) => {
-  const userCreate = await usersService.postUser(req.body);
-
-  res.json(User.toResponse(userCreate));
-});
-router.put('/:id', async (req, res) => {
-  const userUpdate = await usersService.putUser(req.body, req.params.id);
-
-  res.json(User.toResponse(userUpdate));
-});
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
   const userDelete = await usersService.deleteUser(req.params.id);
-  if (userDelete) {
-    res.status(204).end();
-  } else {
-    res.status(404).end();
+  try {
+    if (userDelete) res.status(OK).json(NO_CONTENT);
+    else throw new createError(BAD_REQUEST, 'Bad data');
+  } catch (err) {
+    return next(err);
   }
 });
 module.exports = router;
