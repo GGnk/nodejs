2a94ea667c026e671579875b4b9f67805a991b0b
GGnk <super-nurlan94@yandex.ru>
2020-04-12 16:06:28 +0400

    rafactor: add log function

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
 
+const logs = require('./common/log');
+
+process.on('unhandledRejection', reason => {
+  logs.error(`Unhandled Rejection at promice by reason: ${reason}`);
+});
+
+process.on('uncaughtException', err => {
+  logs.error(`Uncaught Exception ${err}`);
+});
+
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
